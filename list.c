6df49dfff69eaea3933ad4e52cb84e2775fdc927
LeoMirots <leonardo.mirotti@gmail.com>
2018-05-23 17:26:35 +0200

    Added el.h, el.c
    Created to show wich functions to modify when change the type of data of element

--- list.c
+++ list.c
@@ -1,49 +1,5 @@
 #include "list.h"
 
-bool IsEqual(element a, element b)
-{
-	if (a == b)
-		return true;
-	return false;
-}
-
-int cmp(element a, element b)
-{
-	if(a == b)
-		return 0;
-	if(a < b)
-		return -1;
-	return 1;
-}
-
-void AssignElement(element *dst, element src)
-{
-	*dst = src;
-}
-
-int DeleteElement(element e)
-{
-	return EXIT_SUCCESS;
-}
-
-void PrintElement(element e)
-{
-	printf("%u;", e);
-}
-
-list AppendElement(element e, list l)
-{
-	if (l == NULL)
-		return cons(e);
-	while (l->next != NULL)
-		l = tail(l);
-	list c = cons(e);
-	c->root = l->root;
-	l->next = c;
-	return l->root;
-}
-/*---------------------Da qui in poi non modificare--------------------------------------*/
-
 size_t llenght(list l)
 {
 	if (empty(l) == true)
--- list.h
+++ list.h
@@ -3,36 +3,10 @@
 	� sufficiente definirli e modificare la definizione di elemento nella
 	libreria.
 */
-
 #if !defined LIST_H
 #define LIST_H
 
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-typedef element unsigned int							/*Definizione del tipo di dato degli elementi;*/
-
-/*Un oggetto contiene un elemento, l'indirizzo
-dell'elemento succssivo (l->next) e l'indirizzo
-del primo elemento(l->root) della lista*/
-typedef struct list_element {
-	element value;
-	struct list_element *next;
-	struct list_element *root;
-} item;
-
-typedef item* list;
-extern void AssignElement(element *dst, element src);	/*Funzione di assegnamento tra 2 elementi, copia l'elemento src nell'elemento puntato da dst;*/
-extern void PrintElement(element e);					/*Funzione di stampa di un elemento, il parametro � l'elemento da stampare;*/
-extern bool IsEqual(element a, element b);					/*Funzione di confronto tra elementi, riceve due elementi come parametro e ritorna true se sono uguali e false nel caso contrario;*/
-int cmp(element a, element b);				/*Funzione di confronto tra 2 elementi, ritorna 0 se sono uguali, -1 se a < b, 1 se a > b;*/
-extern int DeleteElement(element e);					/*Funzione di cancellazione del dato, occorre modificarla se si cambia il tipo di dato, ritorna 0 se ha successo, 1 nel caso contrario;*/
-extern list AppendElement(element e, list l);			/*Ritorna una lista con aggiunto l'elemento e alla fine;*/
-
-/*-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+#include "el.h"
 
 extern size_t llenght(list l);							/*Riceve come parametro una lista e ritorna il numero di elementi della lista;*/
 extern list cons(element e);							/*Crea una nuova lista inserendo al primo elemento il parametro e, ritorna la lista creata;*/
