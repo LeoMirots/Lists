6c6543eaaad47e0f97fea7906c2b0ad5d26a6548
LeoMirots <leonardo.mirotti@gmail.com>
2018-06-19 17:33:46 +0200

    Added 2 extra typedef for others 2 element data type
    Now is possible to create list with more than 1 data type in the same project; thanks to 2 new data type editable in typedef .... element2, and typedef ...... element3, and all relative functions...

--- el.c
+++ el.c
@@ -1,4 +1,5 @@
 #include "el.h"
+#include "list.h"
 
 bool IsEqual(element a, element b)
 {
@@ -38,6 +39,96 @@ list AppendElement(element e, list l)
 	while (l->next != NULL)
 		l = tail(l);
 	list c = cons(e);
+	c->root = l->root;
+	l->next = c;
+	return l->root;
+}
+
+/*-----------------Element2----------------------------------------------------------------------------------------------------------------------*/
+
+bool IsEqual2(element2 a, element2 b)
+{
+	if (a == b)
+		return true;
+	return false;
+}
+
+int cmp2(element2 a, element2 b)
+{
+	if (a == b)
+		return 0;
+	if (a < b)
+		return -1;
+	return 1;
+}
+
+void AssignElement2(element2 *dst, element2 src)
+{
+	*dst = src;
+}
+
+int DeleteElement2(element2 e)
+{
+	return EXIT_SUCCESS;
+}
+
+void PrintElement2(element2 e)
+{
+	printf("%u;", e);
+}
+
+list2 AppendElement2(element2 e, list2 l)
+{
+	if (l == NULL)
+		return cons2(e);
+	while (l->next != NULL)
+		l = tail2(l);
+	list2 c = cons2(e);
+	c->root = l->root;
+	l->next = c;
+	return l->root;
+}
+
+/*-------------Element3----------------------------------------------------------------------------------------------------------------------------------------*/
+
+bool IsEqual3(element3 a, element3 b)
+{
+	if (a == b)
+		return true;
+	return false;
+}
+
+int cmp3(element3 a, element3 b)
+{
+	if (a == b)
+		return 0;
+	if (a < b)
+		return -1;
+	return 1;
+}
+
+void AssignElement3(element3 *dst, element3 src)
+{
+	*dst = src;
+}
+
+int DeleteElement3(element3 e)
+{
+	return EXIT_SUCCESS;
+}
+
+void PrintElement3(element3 e)
+{
+	printf("%u;", e);
+}
+
+list3 AppendElement3(element3 e, list3 l)
+{
+	if (l == NULL)
+		return cons3(e);
+	while (l->next != NULL)
+		l = tail3(l);
+	list3 c = cons3(e);
 	c->root = l->root;
 	l->next = c;
 	return l->root;
--- el.h
+++ el.h
@@ -7,8 +7,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-typedef element unsigned int							/*Definizione del tipo di dato degli elementi;*/
-
+typedef unsigned int element;							/*Definizione del tipo di dato degli elementi;*/
+typedef bool element2;
+typedef short element3;
 /*Un oggetto contiene un elemento, l'indirizzo
 dell'elemento succssivo (l->next) e l'indirizzo
 del primo elemento(l->root) della lista*/
@@ -18,10 +19,45 @@ typedef struct list_element {
 	struct list_element *root;
 } item;
 
+typedef struct list_element2 {
+	element2 value;
+	struct list_element2 *next;
+	struct list_element2 *root;
+} item2;
+
+typedef struct list_element3 {
+	element3 value;
+	struct list_element3 *next;
+	struct list_element3 *root;
+} item3;
+
 typedef item* list;
+typedef item2 *list2;
+typedef item3 *list3;
+
 extern void AssignElement(element *dst, element src);	/*Funzione di assegnamento tra 2 elementi, copia l'elemento src nell'elemento puntato da dst;*/
 extern void PrintElement(element e);					/*Funzione di stampa di un elemento, il parametro � l'elemento da stampare;*/
 extern bool IsEqual(element a, element b);					/*Funzione di confronto tra elementi, riceve due elementi come parametro e ritorna true se sono uguali e false nel caso contrario;*/
-int cmp(element a, element b);				/*Funzione di confronto tra 2 elementi, ritorna 0 se sono uguali, -1 se a < b, 1 se a > b;*/
+extern int cmp(element a, element b);				/*Funzione di confronto tra 2 elementi, ritorna 0 se sono uguali, -1 se a < b, 1 se a > b;*/
 extern int DeleteElement(element e);					/*Funzione di cancellazione del dato, occorre modificarla se si cambia il tipo di dato, ritorna 0 se ha successo, 1 nel caso contrario;*/
 extern list AppendElement(element e, list l);			/*Ritorna una lista con aggiunto l'elemento e alla fine;*/
+
+/*----------------Element2-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+extern void AssignElement2(element2 *dst, element2 src);	/*Funzione di assegnamento tra 2 elementi, copia l'elemento src nell'elemento puntato da dst;*/
+extern void PrintElement2(element2 e);					/*Funzione di stampa di un elemento, il parametro � l'elemento da stampare;*/
+extern bool IsEqual2(element2 a, element2 b);					/*Funzione di confronto tra elementi, riceve due elementi come parametro e ritorna true se sono uguali e false nel caso contrario;*/
+extern int cmp2(element2 a, element2 b);				/*Funzione di confronto tra 2 elementi, ritorna 0 se sono uguali, -1 se a < b, 1 se a > b;*/
+extern int DeleteElement2(element2 e);					/*Funzione di cancellazione del dato, occorre modificarla se si cambia il tipo di dato, ritorna 0 se ha successo, 1 nel caso contrario;*/
+extern list2 AppendElement2(element2 e, list2 l);			/*Ritorna una lista con aggiunto l'elemento e alla fine;*/
+
+/*----------------Element3----------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+extern void AssignElement3(element3 *dst, element3 src);	/*Funzione di assegnamento tra 2 elementi, copia l'elemento src nell'elemento puntato da dst;*/
+extern void PrintElement3(element3 e);					/*Funzione di stampa di un elemento, il parametro � l'elemento da stampare;*/
+extern bool IsEqual3(element3 a, element3 b);					/*Funzione di confronto tra elementi, riceve due elementi come parametro e ritorna true se sono uguali e false nel caso contrario;*/
+extern int cmp3(element3 a, element3 b);				/*Funzione di confronto tra 2 elementi, ritorna 0 se sono uguali, -1 se a < b, 1 se a > b;*/
+extern int DeleteElement3(element3 e);					/*Funzione di cancellazione del dato, occorre modificarla se si cambia il tipo di dato, ritorna 0 se ha successo, 1 nel caso contrario;*/
+extern list3 AppendElement3(element3 e, list3 l);			/*Ritorna una lista con aggiunto l'elemento e alla fine;*/
+
+#endif //EL_H
